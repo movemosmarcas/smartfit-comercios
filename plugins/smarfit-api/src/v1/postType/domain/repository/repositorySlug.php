3f883c54b7acdf9a0a286dc41e363cc8cc056a1f
joseacordobac <joseacordobac@gmail.com>
2024-08-24 14:28:33 -0500

    API updated

--- plugins/smarfit-api/src/v1/postType/domain/repository/repositorySlug.php
+++ plugins/smarfit-api/src/v1/postType/domain/repository/repositorySlug.php
@@ -19,14 +19,15 @@
           'post_type' => $this->post_type, 
           'name' => $this->repositorySlug)
       );
-      $page_data = array();
+      
+      $all_pages_data = array();
   
       if ($loop->have_posts()) {
           while ($loop->have_posts()) {
               $loop->the_post();
               
               if($this->only_acf !== 'true') {
-                $page_data = array(
+                $page = array(
                   'title'       => get_the_title(),
                   'url'         => get_the_permalink(),
                   'image'       => get_the_post_thumbnail_url(get_the_ID()),
@@ -39,15 +40,16 @@
                   $acf_fields = get_fields();
 
                   if ($acf_fields) {
-                      $page_data['acf'] = $acf_fields;
+                      $page  = array_merge($page, $acf_fields);
                   } else {
-                      $page_data['acf'] = 'No ACF fields found';
+                      $page  = array_merge($page, array('acf' => 'no data found'));
                   }
               }
+              $all_pages_data[] = $page;
           }
-  
+          wp_reset_postdata();
       }
   
-      return $page_data;
+      return $all_pages_data;
   }
 }
