d4f00fa0990765b55815ecd23d0a3362c8b1c299
joseacordobac <joseacordobac@gmail.com>
2024-09-01 17:30:47 -0500

    Update endpoint filters

--- plugins/smarfit-api/src/v1/postType/infrastructure/utilities/recursive-tax.php
+++ plugins/smarfit-api/src/v1/postType/infrastructure/utilities/recursive-tax.php
@@ -1,6 +1,6 @@
 <?php 
 
-function get_childs($slug_tax, $parent_id) {
+function get_childs($slug_tax, $parent_id, $id_post = null) {
 
   $args = array(
     'parent' => $parent_id,
@@ -30,6 +30,28 @@ function get_childs($slug_tax, $parent_id) {
 
 }
 
+function loop_childs($terms, $parent_id) {
+  $new_terms = [];
+
+  foreach ($terms as $value) {
+    if($value->parent == $parent_id) {
+
+      $new_terms[] = [
+        'id' => $value->term_id,
+        'parent' => $value->parent,
+        'name' => $value->name,
+        'slug' => $value->slug,
+        //'childs' => loop_childs($terms, $value->parent)
+      ];
+  
+      
+    }
+
+  }
+
+  return $new_terms;
+}
+
 function get_terms_heraquical($slug_tax, $id_post) {
 
   $terms = wp_get_post_terms($id_post, $slug_tax);
@@ -43,7 +65,7 @@ function get_terms_heraquical($slug_tax, $id_post) {
         'parent' => $value->parent,
         'name' => $value->name,
         'slug' => $value->slug,
-        'childs' => get_childs($slug_tax, $value->term_id)
+        'childs' => loop_childs($terms, $value->term_id)
       ];
   
       
