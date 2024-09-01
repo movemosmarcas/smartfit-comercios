263178a4f3573fad3f049d852b2b57d138d6f33a
joseacordobac <joseacordobac@gmail.com>
2024-09-01 17:31:00 -0500

    Update endpoint filters

--- plugins/smarfit-api/src/v1/postType/infrastructure/utilities/recursive-tax.php
+++ plugins/smarfit-api/src/v1/postType/infrastructure/utilities/recursive-tax.php
@@ -1,35 +1,5 @@
 <?php 
 
-function get_childs($slug_tax, $parent_id, $id_post = null) {
-
-  $args = array(
-    'parent' => $parent_id,
-    'taxonomy' => $slug_tax,
-    'hide_empty' => true
-  );
-
-  $terms = get_terms($args);
-
-  $new_terms = [];
-
-  foreach ($terms as $value) {
-    
-    
-    $new_terms[] = [
-      'id' => $value->term_id,
-      'parent' => $value->parent,
-      'name' => $value->name,
-      'slug' => $value->slug
-    ];
-    
-    $new_terms = array_merge($new_terms, get_childs($slug_tax, $value->term_id));
-
-  }
-
-  return $new_terms;
-
-}
-
 function loop_childs($terms, $parent_id) {
   $new_terms = [];
 
