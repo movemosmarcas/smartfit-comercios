8fcdf36949a1cc42f2b320398acb32eb1688c6d3
joseacordobac <joseacordobac@gmail.com>
2024-10-17 21:33:49 -0500

    update card img - configure favicon

--- themes/smartfit-comercio/header.php
+++ themes/smartfit-comercio/header.php
@@ -11,9 +11,10 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title><?php wp_title(); ?> | Smart Fit </title>
+    <title><?php wp_title(); ?> SmartFit </title>
+    <meta name="robots" content="noindex, nofollow">
     <?php wp_head(); ?>
+    <link rel="icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/icons/favicon.ico" type="image/x-icon" />
 </head>
 
 <body <?php body_class(); ?>>
