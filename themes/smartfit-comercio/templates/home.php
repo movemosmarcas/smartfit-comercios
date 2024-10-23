2ee59371508ff0b75897a3c693d6d72d74af2f45
joseacordobac <joseacordobac@gmail.com>
2024-10-22 23:23:46 -0500

    update smartfit logo

--- themes/smartfit-comercio/templates/home.php
+++ themes/smartfit-comercio/templates/home.php
@@ -15,7 +15,7 @@ get_header(); ?>
   <div class="home-content">
     
     <div class="home__right">
-      <img class="home__logo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/icons/logo.png" alt="SmarFit">
+      <img class="home__logo" src="<?php echo PATH_STYLE; ?>/assets/icons/logo.png" alt="SmarFit">
       <div class="home__form">
         <h1 class="home__form-title">Beneficios Black</h1>
         <div class="home__form-content">
