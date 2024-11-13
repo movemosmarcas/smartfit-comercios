561dafcfd59d43e3fd758932a4831cec65cc4fcd
joseacordobac <joseacordobac@gmail.com>
2024-11-13 00:03:53 -0500

    update plugins img and versions

--- plugins/wp-all-import-pro/addon-api/templates/container-start.php
+++
@@ -1,5 +0,0 @@
-<div class="postbox default rad4 pmxi-addon-group" data-group="<?php echo $group['id']; ?>">
-    <h3 class="postbox-title">
-        <span><?php echo $group['label']; ?></span>
-    </h3>
-    <div class="postbox-content">
