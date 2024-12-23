c1b31a81e4a5df4d13a24c541f20832e18906fc4
inverbots <info@inverbots.com>
2024-12-22 23:54:36 -0500

    update new version codigo server

--- themes/smartfit-comercio/admin/update-codes/option-page/get_data_comercio.php
+++ themes/smartfit-comercio/admin/update-codes/option-page/get_data_comercio.php
@@ -5,51 +5,23 @@
     public function __construct() {}
 
     function get_data_codes() {
-        $args = array(
-            'post_type' => 'comercios',
-            'post_status' => 'publish',
-            'posts_per_page' => -1
-        );
 
-        $posts_list = get_posts($args);
+        global $wpdb;
+        $tabla = $wpdb->prefix . 'reportes';
+
+        $results = $wpdb->get_results("SELECT * FROM $tabla");
 
         $flattened_array = array();
 
-        foreach ($posts_list as $post) {
-            $codigos_repeater = get_field('codigos', $post->ID);
-            $postInformation  = array(
-                'ID' => $post->ID,
-                'title' => sanitize_text_field(get_the_title($post)),
-                'codigo' => get_field('code', $post),
-                'times_use' => get_field('times_gave', $post),
+        foreach ($results as $result) {
+            $flattened_array[] = array(
+                'comercio_name' => $result->comercio_name,
+                'codigo_descuento' => $result->codigo_descuento,
+                'cedula_usuario' => $result->cedula_usuario,
+                'date_created' => $result->date_created
             );
-
-            $flattened_array[] = $postInformation;
-            
-            if ($codigos_repeater && is_array($codigos_repeater)) {
-                foreach ($codigos_repeater as $codigo_item) {
-                    $codigo = isset($codigo_item['codigo']) 
-                              ? (is_array($codigo_item['codigo']) 
-                                  ? implode(', ', $codigo_item['codigo']) 
-                                  : sanitize_text_field($codigo_item['codigo'])) 
-                              : '';
-
-                    $estado = isset($codigo_item['estado']) 
-                              ? (is_array($codigo_item['estado']) 
-                                  ? implode(', ', $codigo_item['estado']) 
-                                  : sanitize_text_field($codigo_item['estado'])) 
-                              : '';
-                    $new_estado = !empty($estado) ? 1 : 0;
-                    $flattened_array[] = array(
-                        'ID' => $post->ID,
-                        'title' => sanitize_text_field(get_the_title($post)),
-                        'codigo' => $codigo,
-                        'times_use' => $new_estado,
-                    );
-                }
-            }
         }
-
+        
         return json_encode($flattened_array);
     }
 }
