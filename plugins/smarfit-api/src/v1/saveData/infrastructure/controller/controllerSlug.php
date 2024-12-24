f12527014b3e15ff6e27ad688f9edb325fc3d2d5
inverbots <info@inverbots.com>
2024-12-24 11:51:36 -0500

    limit

--- plugins/smarfit-api/src/v1/saveData/infrastructure/controller/controllerSlug.php
+++ plugins/smarfit-api/src/v1/saveData/infrastructure/controller/controllerSlug.php
@@ -6,13 +6,13 @@ class ControllerSaveData
   public function apiSlug($param) {
     $method = $param->get_method();
     
-    $limit = control_limit($_SERVER['REMOTE_ADDR']);
-    if ($limit) {
-      return new WP_REST_Response(
-          $limit,
-          429 
-      );
-    }
+    // $limit = control_limit($_SERVER['REMOTE_ADDR']);
+    // if ($limit) {
+    //   return new WP_REST_Response(
+    //       $limit,
+    //       429 
+    //   );
+    // }
 
     if( $method === 'GET') {
       return $this->putCheck($param);
